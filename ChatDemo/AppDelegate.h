fedd7dd9d93d01cc6f936aa217aabb18c685a9ad
云趣科技 <yunqukeji@suntekrddeMacBook-Pro.local>
2017-11-16 17:55:56 +0800

    Initial Commit

---
+++ ChatDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ChatDemo
+//
+//  Created by 云趣科技 on 2017/11/16.
+//  Copyright © 2017年 谢海邦. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ChatDemo/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  ChatDemo
+//
+//  Created by 云趣科技 on 2017/11/16.
+//  Copyright © 2017年 谢海邦. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
